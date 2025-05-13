c6a5a7ded24d49619e5cfebe9582fa71f6babc81
Matteda Ramesh <85834506+rummy143679@users.noreply.github.com>
2025-05-13 21:05:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	this.make = make;
+	this.model = model;
+	this.getMakeModel = () => {
+		return `${this.name} ${this.model}`;
+	}
+}
 
-function SportsCar(make, model, topSpeed) {}
+function SportsCar(make, model, topSpeed) {
+	super(name, model);
+	this.topSpeed = topSpeed;
+	this.getTopSpeed = () => {
+		return this.topSpeed;
+	}
+}
 
 // Do not change the code below
 window.Car = Car;
